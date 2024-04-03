41683506db38de000a9d3dad0861c539280f9c08
bakai <bakai.pochta@gmail.com>
2024-04-03 21:59:58 +0600

    Fill contacts content

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -5,14 +5,16 @@ export const Contacts = () => {
       <div className="contacts__lower"></div>
       <div className="contacts__centered columns__container">
         <div className="columns__column">
-          Feel free to contact me:
-          <p>email</p>
-          <p>phone number</p>
-          <p>telegram</p>
-          <p>linkedIn</p>
+          <p className="contacts__title">Feel free to contact me:</p>
+          <div className="contactEntriesBlock">
+            <p>Email: bakai.pochat@gmail.com</p>
+            <p>Phone number: +996 500 391990</p>
+            <p>Telegram: t.me/bakai_abc</p>
+            <p>LinkedIn: linkedin.com/in/bakai-taalaibek</p>
+          </div>
         </div>
-        <div className="columns__column--centered">
-          Or leave a comment:
+        <div className="columns__column">
+          <p className="contacts__title">Or leave a comment below:</p>
           <p>form</p>
         </div>
       </div>
