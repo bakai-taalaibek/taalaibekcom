d90a1c3feb09e63add7deb22fb1a1590a118fc64
bakai <bakai.pochta@gmail.com>
2024-04-16 20:48:28 +0600

    Fix certificates layout

--- src/components/Certificates.tsx
+++ src/components/Certificates.tsx
@@ -4,7 +4,7 @@ export const Certificates = () => {
   return (
     <div className="certificates">
       <p className="text">My Software Development certificates:</p>
-      <div className="certificates__rowOfImages">
+      <div className="certificates__groupOfImages">
         <ClickableImage src="cs50.png" alt="CS50 certificate" />
         <ClickableImage src="fso.png" alt="Full-Stack Open certificate" />
       </div>
