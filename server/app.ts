a87f121c2bc6f847aa76c10d35060bc4567990e3
bakai <bakai.pochta@gmail.com>
2024-07-23 20:01:42 +0600

    Fix dirname error

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,11 @@ import express from "express"
 import cors from "cors"
 import { join } from "path"
 import logger from "./utils/logger"
+import { fileURLToPath } from "url"
+import { dirname } from "path"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const app = express()
 
