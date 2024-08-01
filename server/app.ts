e3710d40194ce07648a9844cf0584d47a73ab6e7
bakai-taalaibek <bakai.pochta@gmail.com>
2024-08-01 22:56:11 +0600

    Test 3

--- server/app.ts
+++ server/app.ts
@@ -20,7 +20,7 @@ app.get("/ping", (request, response) => {
     "-------------------------------------------------------------------------------------"
   )
   logger.info(request.method, request.body, JSON.stringify(request.headers))
-  response.send("pong-3")
+  response.send("pong-4")
 })
 
 app.get("/*", (_, response) => {
